2b019685927247d71db3dde10bebaeae709ee450
Adi Isfan <isfan.muhammad@gmail.com>
2019-11-29 18:33:34 +0700

    update api 3

---
+++ API Kereta Api/app.py
@@ -0,0 +1,102 @@
+from flask import request, Flask, redirect, jsonify, Response
+import requests
+import json
+from bs4 import BeautifulSoup
+import codecs
+
+app = Flask(__name__)
+pegi2 = "www.pegipegi.com/kereta-api/search/direct/"
+
+
+@app.route('/')
+def test(): 
+	return 'test' 
+
+@app.route('/test')
+def cek() :
+	_url = requests.get('https://www.pegipegi.com/kereta-api/search/direct/GMR/BD/28-11-2019/1/0/EBK')
+		
+	_textdata = BeautifulSoup(_url.text, 'html.parser')
+	
+	
+	return jsonify(_textdata)
+	
+#@app.route('/kereta/<dept>/<dest>/<date>')
+@app.route('/kereta')
+#def getKereta(dept, dest, date):
+def getKereta():
+	try:
+		"""_asal = dept 
+		_tujuan = dest
+		_tanggal = date"""
+		_asal = request.args.get('dept') 
+		_tujuan = request.args.get('dest')
+		_tanggal = request.args.get('date')
+			
+		if _asal == None or _tujuan == None or _tanggal == None: 
+			return(Response('Parameter tidak tepat', 400))
+			
+		_url = requests.get('https://' + pegi2 + _asal + '/' + _tujuan + '/' + _tanggal + '/1/0/EBK')
+		
+		_textdata = BeautifulSoup(_url.text, 'html.parser')
+		
+		"""_error = _textdata.find('tr', attrs = {'class' : 'odd'})
+		
+		if _error:
+			return(Response('Kereta tidak ditemukan', 404))"""
+			
+		
+		_element = _textdata.find('tbody', attrs = {'class' : 'searchResultBody detailOrder active'})
+		_traindata = []
+		_temp = {}
+		_num = 0
+
+		for _tr in _element.find_all('tr'):
+			if _tr.find('td', attrs = {'class' : 'column namaKelasKereta'}) :
+				if _tr.find('div', attrs = {'class' : 'namaKereta'}) :
+					_key = "nama kereta" + str(_num)
+					_value = _tr.find('div', attrs = {'class' : 'namaKereta'}).string
+				_temp[_key] = _value
+				
+				if _tr.find('div', attrs = {'class' : 'kelasKereta'}) :
+					_key = "Kelas" + str(_num)
+					_value = _tr.find('div', attrs = {'class' : 'kelasKereta'}).string
+				_temp[_key] = _value
+				
+			if _tr.find('td', attrs = {'class' : 'column keretaBerangkat'}) :
+				if _tr.find('div', attrs = {'class' : 'time'}) :
+					_key = "Waktu berankat" + str(_num)
+					_value = _tr.find('div', attrs = {'class' : 'time'}).string
+				_temp[_key] = _value
+				
+				if _tr.find('div', attrs = {'class' : 'destiny origresult'}) :
+					_key = "Stasiun asal" + str(_num)
+					_value = _tr.find('div', attrs = {'class' : 'destiny origresult'}).string
+				_temp[_key] = _value
+			
+			if _tr.find('td', attrs = {'class' : 'column keretaTiba'}) :
+				if _tr.find('div', attrs = {'class' : 'time'}) :
+					_key = "Waktu tiba" + str(_num)
+					_value = _tr.find('div', attrs = {'class' : 'time'}).string
+				_temp[_key] = _value
+				
+				if _tr.find('div', attrs = {'class' : 'destiny desresult'}) :
+					_key = "Stasiun Tujuan" + str(_num)
+					_value = _tr.find('div', attrs = {'class' : 'destiny desresult'}).string
+				_temp[_key] = _value
+				
+			_traindata.append(_temp.copy())
+			for key in _temp: 
+				del _temp[key]
+			_num += 1
+		return jsonify(_traindata)
+		
+	except Exception as e:
+		raise e
+		
+if __name__ == '__main__': 
+	app.run()
+			
+		
+		
+		
--- API Tempat/app.py
+++ API Tempat/app.py
@@ -2,7 +2,7 @@ from flask import Flask, Response, redirect, request, jsonify
 from bs4 import BeautifulSoup
 import requests
 import codecs
-#import config
+
 
 
 app = Flask(__name__)
@@ -11,24 +11,14 @@ default_language = 'id' # supported on en (not all language supported)
 region_api = 'http://dev.farizdotid.com/api/daerahindonesia/'
 
 
-# #
-# # Generating JSON responses
-# #
-# def generate_json_response(data, status_code):
-# 	try:
-# 		_response = {
-# 			"data": data
-# 		}
-# 		if (status_code == 400):
-# 			_response = {
-# 				"error": {
-# 					"code": status_code,
-# 					"message": data
-# 				}
-# 			}
-# 		return response
-# 	except Exception as e:
-# 		raise e
+#
+# Generating JSON responses
+#
+def generate_json_response(status_code, response_object):
+	try:
+		pass
+	except Exception as e:
+		raise e
 
 #
 # Authentication token checking
@@ -36,7 +26,7 @@ region_api = 'http://dev.farizdotid.com/api/daerahindonesia/'
 def auth_check(token):
 	try:
 		token = codecs.decode(token, 'hex').decode('utf-8')
-		if token.endswith('alfin'):
+		if token.endswith(config.SECRET_KEY):
 			if token <= 0 or token >= 100000:
 				return Response('Unauthorized', 401)
 			pass
@@ -52,20 +42,29 @@ def auth_check(token):
 #
 @app.route('/')
 def documentation():
-	return redirect('https://app.swaggerhub.com/apis-docs/alfinm01/TST_WikiRegionAPI/1.0.0', code = 303)
+	return redirect('https://app.swaggerhub.com/apis-docs/alfinm01/TST_NationRegionAPI/1.0.0', code = 303)
 
 #
 # Get access token
 # Simple NIM encryption (only for STI ITB student)
 #
+
+@app.route('/test')
+def cek() :
+	_url = requests.get('https://id.wikipedia.org/wiki/Jawa_Barat')
+		
+	_soup = BeautifulSoup(_url.text, 'html.parser')
+	
+	
+	return (_soup)
+	
 @app.route('/token/<nim>')
 def get_token(nim):
 	try:
 		_nim = int(nim)
 		_nim_check = _nim - 18200000
 		if _nim_check > 0 and _nim_check < 100000:
-			_key = 'alfin'
-			_nim = b'str(_nim).join(_alfin)'
+			_nim = b'str(_nim).join(config.SECRET_KEY)'
 			return codecs.encode(_nim, 'hex')
 		else:
 			return Response('Forbidden to access (Invalid NIM)', 403)
@@ -74,37 +73,33 @@ def get_token(nim):
 		raise e
 
 #
-# Get desired region data from Wikipedia
+# Get desired nation data from Wikipedia
 #
-@app.route('/wiki')
-def wiki():
+@app.route('/nation')
+def nation():
 	try:
-		# Authentication check
-		#token = request.headers.get('token')
-		#auth_check(token)
-
 		# Get Wikipedia content
-		_wiki = request.args.get('name')
-		if _wiki is None:
-			return Response('Must include wiki query', 400)
+		_nation = request.args.get('name')
+		if _nation is None:
+			return Response('Must include nation query', 400)
 		_language = request.args.get('language')
 		if _language:
-			_result = requests.get('https://' + _language + '.' + wikipedia + _wiki)
+			_result = requests.get('https://' + _language + '.' + wikipedia + _nation)
 		else:
-			_result = requests.get('https://' + default_language + '.' + wikipedia + _wiki)
+			_result = requests.get('https://' + default_language + '.' + wikipedia + _nation)
 		_soup = BeautifulSoup(_result.text, 'html.parser')
 
 		# Wiki not found
 		_not_found = _soup.find('div', attrs = {'class' : 'noarticletext'})
 		if _not_found:
-			return Response('Wiki not found', 404)
+			return Response('Nation not found', 404)
 		
 		# Parsing from HTML to JSON
 		_table = _soup.find('table', attrs = {'class' : 'infobox'})
-
-		# Wrong wiki query
+		
+		# Wrong nation query
 		if _table == None:
-			return Response('Wiki query is wrong', 404)
+			return Response('Nation query is wrong', 404)
 
 		# Iterate every tr
 		_tbody = _table.find('tbody')
@@ -118,13 +113,13 @@ def wiki():
 
 			# Parsing dictionary key
 			if _tr.find('a'):
-				_key = _tr.find('a').text
+				_key = _tr.find('a').string
 			else:
 				if _tr.find('b'):
-					_key = _tr.find('b').text
+					_key = _tr.find('b').string
 				else:
 					if _tr.find('div'):
-						_key = _tr.find('div').text
+						_key = _tr.find('div').string
 					else:
 						_key = 'uncrawled object ' + str(_counter)
 
@@ -143,56 +138,9 @@ def wiki():
 	except Exception as e:
 		raise e
 
-#
-# Get desired region data from Wikipedia in form of raw data
-#
-@app.route('/wiki/raw')
-def wiki_raw():
-	try:
-		# Authentication check
-		#token = request.headers.get('token')
-		#auth_check(token)
 
-		# Get Wikipedia content
-		_wiki = request.args.get('name')
-		if _wiki is None:
-			return Response('Must include wiki query', 400)
-		_language = request.args.get('language')
-		if _language:
-			_result = requests.get('https://' + _language + '.' + wikipedia + _wiki)
-		else:
-			_result = requests.get('https://' + default_language + '.' + wikipedia + _wiki)
-		_soup = BeautifulSoup(_result.text, 'html.parser')
-
-		# Wiki not found
-		_not_found = _soup.find('div', attrs = {'class' : 'noarticletext'})
-		if _not_found:
-			return Response('Wiki not found', 404)
-		
-		# Find table
-		_table = _soup.find('table', attrs = {'class' : 'infobox'})
-
-		# Wrong wiki query
-		if _table == None:
-			return Response('Wiki query is wrong', 404)
-
-		# Find first paragraph
-		_div = _soup.find('div', attrs = {'class' : 'mw-parser-output'})
-		_p = _soup.find('p')
-
-		# Wrong wiki query
-		if _p == None:
-			return Response('Wiki query is wrong', 404)
-
-		# Iterate every tr
-		_data = {}
-		_data['table'] = str(_table)
-		_data['paragraph'] = str(_p)
-
-		return jsonify(_data)
-
-	except Exception as e:
-		raise e
+# Route below is using direct method (without database storage)
+# Possibility of change in the future
 
 #
 # Get Indonesian provinces data
@@ -200,9 +148,6 @@ def wiki_raw():
 @app.route('/id/province')
 def province():
 	try:
-		# Authentication check
-		#token = request.headers.get('token')
-		#auth_check(token)
 
 		# Reformat JSON data
 		_result = requests.get(region_api + 'provinsi').json()
@@ -219,10 +164,6 @@ def province():
 @app.route('/id/city/<province_id>')
 def city(province_id):
 	try:
-		# Authentication check
-		#token = request.headers.get('token')
-		#auth_check(token)
-
 		# Reformat JSON data
 		_result = requests.get(region_api + 'provinsi/' + province_id + '/kabupaten').json()
 		_data = _result['kabupatens']
@@ -237,10 +178,6 @@ def city(province_id):
 @app.route('/id/district/<city_id>')
 def district(city_id):
 	try:
-		# Authentication check
-		#token = request.headers.get('token')
-		#auth_check(token)
-
 		# Reformat JSON data
 		_result = requests.get(region_api + 'provinsi/kabupaten/' + city_id + '/kecamatan').json()
 		_data = _result['kecamatans']
@@ -255,9 +192,6 @@ def district(city_id):
 @app.route('/id/village/<district_id>')
 def village(district_id):
 	try:
-		# Authentication check
-		#token = request.headers.get('token')
-		#auth_check(token)
 
 		# Reformat JSON data
 		_result = requests.get(region_api + 'provinsi/kabupaten/kecamatan/' + district_id + '/desa').json()
@@ -269,6 +203,5 @@ def village(district_id):
 
 
 if __name__ == '__main__':
-	app.run(threaded = True, port = 5000)
-    #app.run(ssl_context = ('cert.pem', 'key.pem'))
+    app.run()
     #app.run(debug = True)
