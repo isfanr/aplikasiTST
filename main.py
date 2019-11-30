d177cd1caf32ba4e19e0e8066a552f19e085ec25
Adi Isfan <isfan.muhammad@gmail.com>
2019-12-01 01:28:49 +0700

    update container 1 API + fix form

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
@@ -6,21 +6,22 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     #API tempat
-    data = requests.get('http://localhost:4000/id/city/32').json() #hanya jawa barat
+    data = requests.get('https://wiki-region-api.herokuapp.com/id/city/32').json() #hanya jawa barat
     return render_template("home.html", kota = data)
     
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-@app.route("/hasil")
+@app.route("/hasil", methods=['GET'])
 def hasil():
     #API kereta api
-    asal = "BD" #ini harusnya diisi sama kode stasiun yang ditranslate dari daerah (dinamis)
+    asal = "GMR"
     tujuan = "GMR" #ini harusnya diisi sama kode stasiun yang ditranslate dari daerah (dinamis)
     tanggal = "05-12-2019" #ini harusnya diisi sama penggabungan tanggal hari tahun di laman home (dinamis)
-    
-    data = requests.get('http://localhost:3000/kereta?dept=' + asal + '&dest=' + tujuan + '&date=' + tanggal').json()
+    # asal = request.form['AsalKota']
+
+    data = requests.get('http://localhost:3000/kereta?dept=' + asal + '&dest=' + tujuan + '&date=' + tanggal).json()
     return render_template("hasil.html", submit = data)
 
 if __name__ == "__main__":
